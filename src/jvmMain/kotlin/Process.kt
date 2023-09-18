e080ad382c4691346c0b7254482cb9179d6f4197
kouheisatou <koheisato0420@gmail.com>
2023-09-18 09:52:26 +0900

    FIX : loading ui alignment center

--- src/jvmMain/kotlin/Process.kt
+++ src/jvmMain/kotlin/Process.kt
@@ -1,10 +1,8 @@
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -23,32 +21,34 @@ class Process {
 }
 
 @Composable
-fun ProgressBarComponent(progress: Float, processName: String){
+fun ProgressBarComponent(progress: Float, processName: String) {
 
     val textMeasurer = rememberTextMeasurer()
     var size by remember { mutableStateOf(IntSize.Zero) }
-    Box(
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(10.dp)
-            .onSizeChanged { size = it }
-            .drawBehind {
-                drawText(
-                    text = "${floor(progress * 10000) / 100}%",
-                    textMeasurer = textMeasurer,
-                    topLeft = Offset(x = size.width.toFloat() * progress, y = 0f),
-                    maxLines = 1,
-                )
-            },
     ) {
-        Column {
 
-            LinearProgressIndicator(
-                modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp),
-                progress = progress
-            )
-            Text(processName)
-        }
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+                .drawBehind {
+                    drawText(
+                        text = "${floor(progress * 10000) / 100}%",
+                        textMeasurer = textMeasurer,
+                        topLeft = Offset(x = size.width.toFloat() * progress, y = 0f),
+                        maxLines = 1,
+                    )
+                }
+                .padding(vertical = 10.dp)
+                .onSizeChanged { size = it },
+            progress = progress
+        )
+        Text(processName, maxLines = 1)
     }
 }
 
